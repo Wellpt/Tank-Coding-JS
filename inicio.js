3c8852768a500a50d5af828f5de0fb09eff7a341
Wellington Nobre <wellington.multilaser@gmail.com>
2022-09-02 16:10:45 -0300

    update

---
+++ inicio.js
@@ -0,0 +1,9 @@
+let nome = ("Wellington Nobre Mendes Quaresma");
+let anoAtual = 2022; 
+let anoNasc = 1984;
+let cidade = ("Niterói");
+let sobre = (" Sou apaixonado por programação, nascido de família simples, só pude estudar após conquistar independência financeira..");
+
+let idade = ( anoAtual - anoNasc);
+
+console.log (`Ola eu sou ${nome}, Tenho ${idade} anos de idade. Um pouco sobre mim: ${sobre} `);
